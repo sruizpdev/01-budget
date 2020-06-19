08222f4679556da867c154aaadcc330a4c080930
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-06-19 13:51:28 +0200

    shortid added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,41 @@
 import React, { useState } from 'react';
+import Error from './Error';
+import shortid from 'shortid';
+
 const Form = () => {
+  const [name, saveName] = useState('');
+  const [quantity, saveQuantity] = useState(0);
+  const [error, saveError] = useState(false);
+
+  const addExpense = (e) => {
+    e.preventDefault();
+    if (quantity < 1 || isNaN(quantity) || name.trim() === '') {
+      saveError(true);
+      return;
+    }
+    saveError(false);
+    const expense = {
+      name,
+      quantity,
+      id: shortid.generate(),
+    };
+    console.log(expense);
+  };
+
   return (
-    <form>
+    <form onSubmit={addExpense}>
       <h2>Expenses here</h2>
+      {error ? (
+        <Error message="Both fields are required or wrong quantity" />
+      ) : null}
       <div className="field">
         <label>Expense name</label>
         <input
           type="text"
           className="u-full-width"
           placeholder="Example: transport"
+          value={name}
+          onChange={(e) => saveName(e.target.value)}
         />
       </div>
       <div className="field">
@@ -17,6 +44,8 @@ const Form = () => {
           type="number"
           className="u-full-width"
           placeholder="Example: 300"
+          value={quantity}
+          onChange={(e) => saveQuantity(parseInt(e.target.value))}
         />
       </div>
       <input
